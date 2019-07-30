ccda33b7bd0072188de865d02a9d94208656f38a
Marcio Bruno Rufino de Oliveira <brunomarcio@gmail.com>
2019-07-30 15:20:11 -0300

    aqui coloco uma mensagem, tipo: comitando primeiro post

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname,"public")))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/View/index.html')
+})
+
+app.get('/artigos', (req, res) => {
+    res.sendFile(__dirname+'/View/artigos.html')
+})
+
+app.get('/eventos', (req, res) => {
+    res.sendFile(__dirname+'/View/listEventos.html')
+})
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+    console.log('Umbler listening on port %s', port);
+});
