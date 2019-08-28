63060c137a15e9ba49521f0661ac73d7c349903a
Marcio Bruno Rufino de Oliveira <marcio.b@academico.ifrn.edu.br>
2019-08-28 16:44:29 -0300

    show

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 app.use(express.static(path.join(__dirname,"public")))
+
+app.use(express.static('.'))
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname+'/view/index.html')
