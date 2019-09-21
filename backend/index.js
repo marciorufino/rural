00b6f7528a59dd22ececddb914ba60ca27df8535
Marcio Bruno Rufino de Oliveira <brunomarcio@gmail.com>
2019-09-21 16:16:53 -0300

    fgdgfgvg

--- backend/index.js
+++ backend/index.js
@@ -24,17 +24,8 @@ app.get('/eventos', (req, res) => {
     res.sendFile(__dirname+'/view/listEventos.html')
 })
 
-<<<<<<< HEAD
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname+'/view/login.html')
-})
-
-app.get('/cadastro-publicacoes', (req, res) => {
-    res.sendFile(__dirname+'/view/formulario-publicacoes.html')
-=======
 app.get('/tempo', (req, res) => {
     res.sendFile(__dirname+'/view/previsao-tempo.html')
->>>>>>> d5e85b38ea26182cb6b8cad3aa22ea6ebe66e2e4
 })
 
 var port = process.env.PORT || 3000;
